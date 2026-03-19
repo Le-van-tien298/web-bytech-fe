da57cefaac79488bcc822b676eb4d733a3746596
Tien <tien298@gmail.com>
2026-03-19 12:21:18 +0700

    setup version2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <h1 className={styles.title}>Welcome to Web-Bytech!</h1>
+
     </main>
   );
 }
